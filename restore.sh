a57842a3d4ef4b58ceed7355aadbd106178abd75
peter@rimuhosting.com <peter@rimuhosting.com>
2022-08-02 18:09:32 +1200

    restore improvements

--- restore.sh
+++ restore.sh
@@ -9,6 +9,7 @@ newip="${newip:-127.0.0.4}"
 
 # gzip file we'll be restoring from
 archivegz="${archivegz:-/root/s2i.gz}"
+archivegz="$(realpath "$archivegz")"
 
 # location we will be restoring to, typically /
 restoretopath="${restoretopath:-/}"
@@ -24,6 +25,12 @@ while true; do
   systemctl stop postfix
   
   restorescratchdir="$(find /root/restore.* -maxdepth 0 -mtime -10 -type d  | head)"
+  if [ ! -z "$restorescratchdir" ]; then
+    if [ $(find "$restorescratchdir" -type f | head -n 400 | wc -l) -lt 300 ]; then
+      echo "The pre-existing restore directory ($restorescratchdir) does not appear to be complete.  Ignoring."
+      restorescratchdir=""
+    fi  
+  fi
   if [ ! -z "$restorescratchdir" ]; then 
     echo "Restoring from pre-existing restore directory: $restorescratchdir"
   else
@@ -32,10 +39,12 @@ while true; do
     mkdir -p $restorescratchdir
     cd $restorescratchdir
     echo "Extracting backup from $archivegz $(ls -lh $archivegz) to restore directory $restorescratchdir"
-    tar xzf $archivegz
+    tar xzf "$archivegz"
+    [ $? -ne 0 ] && ret=1 && break
   fi
   [ -z "$restorescratchdir" ] && echo "no restore directory set" >&2 && ret=1 && break 
-rsync --delete --archive --hard-links --acls --xattrs --perms --executability --acls --owner --group --specials --times --numeric-ids --ignore-errors  \
+   # --force-change for immutables, but does not work on some distros. e.g. 311/2014
+rsync --delete --archive --hard-links --acls --xattrs --perms --executability --acls --owner --group --specials --times --numeric-ids --ignore-errors \
 --exclude 'etc/network/interfaces' \
 --exclude=root/backup* \
 --exclude=root/s2i* \
@@ -60,6 +69,10 @@ rsync --delete --archive --hard-links --acls --xattrs --perms --executability --
 --exclude=home/*/.cache \
 --exclude=home/*/.local/share/Trash \
 $restorescratchdir/* "${restoretopath:-/}" 2>&1 | tee -a rsync.log
+  if [ ${PIPESTATUS[0]} -ne 1 ] ; then 
+    ret=1
+    break
+  fi
   if [  -z "$oldip" ]; then 
     echo "No oldip provided, not searching and replacing for that."
   elif [ -z "$newip" ]; then
@@ -71,6 +84,7 @@ $restorescratchdir/* "${restoretopath:-/}" 2>&1 | tee -a rsync.log
     #/etc/apache2/sites-available/000-default.conf:NameVirtualHost 127.0.0.3:443
     find /etc/ -type f | xargs grep -l "$oldip" | grep -v hosts | xargs --no-run-if-empty replace "$oldip" "$newip" --
    fi
+   break
 done
 # set an exit code
 if [ $ret -ne 0 ]; then 
