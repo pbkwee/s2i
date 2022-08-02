61120da2d9e165550c9a618201e4d2cc24ef1257
peter@rimuhosting.com <peter@rimuhosting.com>
2022-08-02 17:49:03 +1200

    restore improvements

--- restore.sh
+++ restore.sh
@@ -4,31 +4,37 @@
 # https://rimuhosting.com
 
 #change these to your IP.  old = original host's IP.  new = the IP on the server we are restoring to
-oldip=127.0.0.3
-newip=127.0.0.4
-archivegz=/root/s2i.gz
+oldip="${oldip:-127.0.0.3}"
+newip="${newip:-127.0.0.4}"
+
+# gzip file we'll be restoring from
+archivegz="${archivegz:-/root/s2i.gz}"
+
+# location we will be restoring to, typically /
+restoretopath="${restoretopath:-/}"
+
 ret=0
 while true; do 
-  ! grep -q  backup <(hostname) && echo 'hostname is not backup, exiting' && ret=1 && break
+  ! grep -q  backup <(hostname) && echo 'hostname does not contain the "backup", exiting as a safety precaution.  If this is the right host, set a hostname like: hostname backup.example.com' && ret=1 && break
   [ 0 -ne $(netstat -ntp | egrep -v ':22|Foreign Address|Active Internet connections' | wc -l) ] && echo "There are some non ssh connections.  wrong server?  exiting." && netstat -ntp && ret=1 && break
-  [ ! -f $archivegz ] && echo "no backup file '$archivegz', exiting" && ret=1 && break
   # Stop services not needed
   systemctl stop fail2ban
   systemctl stop apache2
   systemctl stop mysql
   systemctl stop postfix
   
-  restoredir="$(find /root/restore.* -maxdepth 0 -mtime -10 -type d  | head)"
-  if [ ! -z "$restoredir" ]; then 
-    echo "Restoring from pre-existing restore directory: $restoredir"
+  restorescratchdir="$(find /root/restore.* -maxdepth 0 -mtime -10 -type d  | head)"
+  if [ ! -z "$restorescratchdir" ]; then 
+    echo "Restoring from pre-existing restore directory: $restorescratchdir"
   else
-    restoredir="/root/restore.$$"
-    mkdir $restoredir
-    cd $restoredir
-    echo "Extracting backup to restore directory $restoredir"
+    [ ! -f "$archivegz" ] && echo "no backup file '$archivegz', exiting" && ret=1 && break
+    restorescratchdir="/root/restore.$$"
+    mkdir -p $restorescratchdir
+    cd $restorescratchdir
+    echo "Extracting backup from $archivegz $(ls -lh $archivegz) to restore directory $restorescratchdir"
     tar xzf $archivegz
   fi
-  [ -z "$restoredir" ] && echo "no restore directory set" >&2 && ret=1 && break 
+  [ -z "$restorescratchdir" ] && echo "no restore directory set" >&2 && ret=1 && break 
 rsync --delete --archive --hard-links --acls --xattrs --perms --executability --acls --owner --group --specials --times --numeric-ids --ignore-errors  \
 --exclude 'etc/network/interfaces' \
 --exclude=root/backup* \
@@ -53,7 +59,7 @@ rsync --delete --archive --hard-links --acls --xattrs --perms --executability --
 --exclude=home/*/.gvfs \
 --exclude=home/*/.cache \
 --exclude=home/*/.local/share/Trash \
-$restoredir/* / 2>&1 | tee -a rsync.log
+$restorescratchdir/* "${restoretopath:-/}" 2>&1 | tee -a rsync.log
   if [  -z "$oldip" ]; then 
     echo "No oldip provided, not searching and replacing for that."
   elif [ -z "$newip" ]; then
@@ -66,4 +72,9 @@ $restoredir/* / 2>&1 | tee -a rsync.log
     find /etc/ -type f | xargs grep -l "$oldip" | grep -v hosts | xargs --no-run-if-empty replace "$oldip" "$newip" --
    fi
 done
-exit $ret
+# set an exit code
+if [ $ret -ne 0 ]; then 
+  false;
+else 
+  true
+fi
